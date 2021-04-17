c01946f9caba0dedd2836045b66bfcb2cda8b9db
devallever <devallever@163.com>
2021-04-17 23:28:38 +0800

    Camera: 优化预览大小算法

--- camera/src/main/java/com/allever/android/lib/camera/CameraProxyImpl.kt
+++ camera/src/main/java/com/allever/android/lib/camera/CameraProxyImpl.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.ImageFormat
+import android.graphics.Point
 import android.hardware.Camera
+import android.hardware.Camera.CameraInfo
 import android.view.Surface
 import android.view.SurfaceView
 import android.view.View
@@ -14,7 +16,9 @@ import com.allever.android.lib.camera.core.CameraManager
 import com.allever.android.lib.camera.core.ICameraProxy
 import com.allever.android.lib.camera.core.Size
 import java.lang.ref.WeakReference
+import java.util.*
 import java.util.concurrent.Executors
+import kotlin.math.abs
 
 class CameraProxyImpl : ICameraProxy {
 
@@ -30,6 +34,13 @@ class CameraProxyImpl : ICameraProxy {
     private var mCameraId = Camera.CameraInfo.CAMERA_FACING_BACK
 
     private lateinit var mPreviewRef: WeakReference<View>
+
+    /**
+     * 额外的旋转角度（用于适配一些定制设备）
+     */
+    private var additionalRotation = 0
+
+    private var previewSize: Size? = null
 
     private val mPreviewCallback = Camera.PreviewCallback { data, camera ->
 //        log("onPreviewFrame")
@@ -93,6 +104,13 @@ class CameraProxyImpl : ICameraProxy {
 
             //获取参数
             val params = camera.parameters
+
+            //最佳大小
+            previewSize = Size(1920, 1080)
+            val bestSupportSize = getBestSupportedSize(params.supportedPreviewSizes, Point(1920, 1080))
+            log("最佳大小 = ${bestSupportSize.width} x ${bestSupportSize.height}")
+            params.setPreviewSize(bestSupportSize.width, bestSupportSize.height)
+            params.setPictureSize(bestSupportSize.width, bestSupportSize.height)
 
             //设置预览格式
             params.previewFormat = ImageFormat.NV21
@@ -175,27 +193,29 @@ class CameraProxyImpl : ICameraProxy {
     }
 
     override fun getDisplayOrientation(context: Context?, cameraId: Int): Int {
-        val info = Camera.CameraInfo()
-        log("getDisplayOrientation: cameraId = $cameraId")
-        Camera.getCameraInfo(cameraId, info)
         val windowManager = context?.getSystemService(Context.WINDOW_SERVICE) as? WindowManager
         val rotation = windowManager?.defaultDisplay?.rotation
-        var degrees = 0
+        var degrees = rotation!! * 90
         when (rotation) {
             Surface.ROTATION_0 -> degrees = 0
             Surface.ROTATION_90 -> degrees = 90
             Surface.ROTATION_180 -> degrees = 180
             Surface.ROTATION_270 -> degrees = 270
+            else -> {
+            }
         }
+        additionalRotation /= 90
+        additionalRotation *= 90
+        degrees += additionalRotation
         var result: Int
-        //前置
-        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+        val info = CameraInfo()
+        Camera.getCameraInfo(mCameraId, info)
+        if (info.facing == CameraInfo.CAMERA_FACING_FRONT) {
             result = (info.orientation + degrees) % 360
             result = (360 - result) % 360
         } else {
             result = (info.orientation - degrees + 360) % 360
-        }//后置
-
+        }
         return result
     }
 
@@ -227,4 +247,61 @@ class CameraProxyImpl : ICameraProxy {
     private fun getCameraId(): Int {
         return Camera.CameraInfo.CAMERA_FACING_BACK;
     }
+
+    private fun getBestSupportedSize(
+        sizes: List<Camera.Size>,
+        previewViewSize: Point?
+    ): Camera.Size {
+        var sizes: List<Camera.Size>? = sizes
+        if (sizes == null || sizes.isEmpty()) {
+            return mCamera!!.parameters.previewSize
+        }
+        val tempSizes =
+            sizes.toTypedArray()
+        Arrays.sort(
+            tempSizes
+        ) { o1, o2 ->
+            if (o1.width > o2.width) {
+                -1
+            } else if (o1.width == o2.width) {
+                if (o1.height > o2.height) -1 else 1
+            } else {
+                1
+            }
+        }
+        sizes = listOf(*tempSizes)
+        var bestSize = sizes[0]
+        var previewViewRatio: Float
+        previewViewRatio = if (previewViewSize != null) {
+            previewViewSize.x.toFloat() / previewViewSize.y.toFloat()
+        } else {
+            bestSize.width.toFloat() / bestSize.height.toFloat()
+        }
+        if (previewViewRatio > 1) {
+            previewViewRatio = 1 / previewViewRatio
+        }
+        val isNormalRotate = additionalRotation % 180 == 0
+        for (s in sizes) {
+            if (previewSize != null && previewSize?.width == s.width && previewSize?.height == s.height) {
+                return s
+            }
+            if (isNormalRotate) {
+                if (abs(s.height / s.width.toFloat() - previewViewRatio) < abs(
+                        bestSize.height / bestSize.width.toFloat() - previewViewRatio
+                    )
+                ) {
+                    bestSize = s
+                }
+            } else {
+                if (abs(s.width / s.height.toFloat() - previewViewRatio) < abs(
+                        bestSize.width / bestSize.height.toFloat() - previewViewRatio
+                    )
+                ) {
+                    bestSize = s
+                }
+            }
+        }
+        return bestSize
+    }
+
 }
